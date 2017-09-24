fae253a312380b062e93ac594661841007e4ab47
Mihir Naik <mdnaik17@gmail.com>
2017-09-24 10:31:58 -0700

    controllers setup and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  root 'properties#index'
+  resources :users
+  resources :properties
+  resources :sessions, only: [:new, :create]
+  delete '/logout' => 'sessions#destroy', as: :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
