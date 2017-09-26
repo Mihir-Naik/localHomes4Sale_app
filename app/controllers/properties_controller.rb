527de9e3d259bcee5c901b070ae5c0ad9b7d59c7
Mihir Naik <mdnaik17@gmail.com>
2017-09-25 18:03:50 -0700

    Property index, new, create and show works

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,14 +1,26 @@
 class PropertiesController < ApplicationController
+
+  before_action :authorize, except: [:index, :show]
+
   def index
+    @properties = Property.all
   end
 
   def new
+    @property = Property.new
   end
 
   def show
+    @property = Property.find(params[:id])
   end
 
   def create
+    @property = current_user.properties.new(property_params)
+    if @property.save
+      redirect_to user_path(@current_user)
+    else
+      redirect to new_property_path
+    end
   end
 
   def edit
@@ -18,5 +30,11 @@ class PropertiesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:price, :address, :sq_footage, :no_of_bedroom, :no_of_bathroom, :description, :image)
   end
 end
